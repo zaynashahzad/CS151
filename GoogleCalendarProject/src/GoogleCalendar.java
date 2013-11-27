ce4f45b86867a1531828447af224e8db68985e31
zaynashahzad <zayna@zayna.gateway.2wire.net>
2013-11-26 20:33:00 -0800

    Fixes the exception we currently get =)

--- GoogleCalendarProject/src/GoogleCalendar.java
+++ GoogleCalendarProject/src/GoogleCalendar.java
@@ -4,6 +4,8 @@ import java.awt.*;
 public class GoogleCalendar {
 
     public static void main(String[] args) {
+        System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+
 //        Events events = new Events();
 
         CalendarFrame cf = new CalendarFrame();
