4d4a098106c17a93253241c583b50478d193d632
zaynashahzad <zayna@zayna.gateway.2wire.net>
2013-11-26 21:30:57 -0800

    Since DayView implements JFrame, we should keep all other views consistent with that. Changing JPanels to Jframes.

--- GoogleCalendarProject/src/WeekView.java
+++ GoogleCalendarProject/src/WeekView.java
@@ -1,10 +1,11 @@
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 
 
-public class WeekView  extends JPanel implements ChangeListener{
+public class WeekView  extends JFrame implements ChangeListener{
     public void stateChanged(ChangeEvent e) {
 
     
