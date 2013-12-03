3bcf80f401c0b63adf2d9966db9ef394eb498772
zaynashahzad <zayna@zayna.gateway.2wire.net>
2013-12-02 19:27:09 -0800

    DayColorConcrete javadoc complete

--- GoogleCalendarProject/src/DayColorConcrete.java
+++ GoogleCalendarProject/src/DayColorConcrete.java
@@ -7,7 +7,9 @@
  * @version 1.01 2013/12/02
  */
 import java.awt.Color;
-
+/**
+ * DayColorConcrete is used by DayView to determine what color to paint jlabels
+ */
 public class DayColorConcrete implements ColorInterface {
 
     private Color c = null;
@@ -16,10 +18,17 @@ public class DayColorConcrete implements ColorInterface {
         pickColor();
     }
 
+    /**
+     * Set the color to a blue-ish tint
+     */
     public void pickColor() {
         c = new Color(135, 206, 250);
     }
     
+    /**
+     * Gets the current color to use
+     * @return the current color
+     */
     public Color getColor(){
         return c;
     }
