09588881c6a6d79ea9bab558559b50931743357c
zaynashahzad <zayna@zayna.gateway.2wire.net>
2013-12-02 19:41:50 -0800

    DayEvents javadocs complete

--- GoogleCalendarProject/src/DayEvents.java
+++ GoogleCalendarProject/src/DayEvents.java
@@ -8,12 +8,22 @@
  */
 import java.util.Date;
 
+/**
+ * DayEvents holds the data for one event in calendar
+ */
 public class DayEvents {
     private String eventName;
     private int startHour; 
     private int endHour;
     private Date startDate;
     
+    /**
+     * Makes a new event with the given parameters
+     * @param name the event name
+     * @param start the start hour of event
+     * @param end the end hour of event
+     * @param date the event date
+     */
     public DayEvents(String name, int start, int end, Date date){
         eventName = name; 
         startHour = start; 
@@ -21,21 +31,42 @@ public class DayEvents {
         startDate = date;
     }
     
+    /**
+     * Gets the event name
+     * @return the event name
+     */
     public String getName(){
         return eventName;
     }
 
+    /**
+     * Gets the events start hour (0-23)
+     * @return the events start hour
+     */
     public int getStartHour(){
         return startHour;
     }
     
+    /**
+     * Gets the events end hour (0-23)
+     * @return the events end hour
+     */
     public int getEndHour(){
         return endHour;
     }
 
+    /**
+     * Gets the date that event is on the calendar for
+     * @return The scheduled date of event
+     */
     public Date getDate(){
         return startDate;
     }
+    
+    /**
+     * Gets a string representation of the event object
+     * @return a string of event details
+     */
     public String toString(){
         return eventName + " " + startDate + " " + startHour + " " + endHour;
     }
